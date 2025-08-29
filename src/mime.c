8ab4aa7597751c6e67a0793157564ef9ac20d513
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-08-29 05:38:11 +0500

    fix(mime.c): integrate hashtable for O(1) lookup

--- src/mime.c
+++ src/mime.c
@@ -1,15 +1,19 @@
 #include <string.h>
 #include <ctype.h>
 #include "mime.h"
+#include "hashtable.h"
 
 #define DEFAULT_MIME_TYPE "application/octet-stream"
+
+struct hashtable *ht = NULL;
 
 /**
  * Lowercase a string
  */
 char *strlower(char *s)
 {
-    for (char *p = s; *p != '\0'; p++) {
+    for (char *p = s; *p != '\0'; p++)
+    {
         *p = tolower(*p);
     }
 
@@ -21,26 +25,38 @@ char *strlower(char *s)
  */
 char *mime_type_get(char *filename)
 {
+    if (ht == NULL)
+    {
+        ht = hashtable_create(0, NULL);
+        hashtable_put(ht, "html", "text/html");
+        hashtable_put(ht, "htm", "text/html");
+        hashtable_put(ht, "jpeg", "image/jpg");
+        hashtable_put(ht, "jpg", "image/jpg");
+        hashtable_put(ht, "css", "text/css");
+        hashtable_put(ht, "js", "application/javascript");
+        hashtable_put(ht, "json", "application/json");
+        hashtable_put(ht, "txt", "text/plain");
+        hashtable_put(ht, "gif", "image/gif");
+        hashtable_put(ht, "png", "image/png");
+    }
+
+
     char *ext = strrchr(filename, '.');
 
-    if (ext == NULL) {
+    if (ext == NULL)
+    {
         return DEFAULT_MIME_TYPE;
     }
-    
+
     ext++;
 
     strlower(ext);
 
-    // TODO: this is O(n) and it should be O(1)
+    char *file_type = hashtable_get(ht, ext);
+    if (!file_type)
+    {
+        return DEFAULT_MIME_TYPE;
+    }
 
-    if (strcmp(ext, "html") == 0 || strcmp(ext, "htm") == 0) { return "text/html"; }
-    if (strcmp(ext, "jpeg") == 0 || strcmp(ext, "jpg") == 0) { return "image/jpg"; }
-    if (strcmp(ext, "css") == 0) { return "text/css"; }
-    if (strcmp(ext, "js") == 0) { return "application/javascript"; }
-    if (strcmp(ext, "json") == 0) { return "application/json"; }
-    if (strcmp(ext, "txt") == 0) { return "text/plain"; }
-    if (strcmp(ext, "gif") == 0) { return "image/gif"; }
-    if (strcmp(ext, "png") == 0) { return "image/png"; }
-
-    return DEFAULT_MIME_TYPE;
+    return file_type;
 }
